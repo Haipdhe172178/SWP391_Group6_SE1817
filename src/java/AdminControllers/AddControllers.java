74a035789c6fe91e681b9a3433ecb32c5c1b3d0f
PhamQuangHuy <huypqhe171458@fpt.edu.vn>
2024-05-27 15:47:40 +0700

    Dashboard fix lan 2

--- src/java/AdminControllers/AddControllers.java
+++ src/java/AdminControllers/AddControllers.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package AdminControllers;
 
 import DAL.AuthorDao;
@@ -29,41 +28,44 @@ import java.util.List;
  * @author huyca
  */
 public class AddControllers extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet AddControllers</title>");  
+            out.println("<title>Servlet AddControllers</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet AddControllers at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet AddControllers at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         CategoryDao categoryDao = new CategoryDao();
         AuthorDao authorDao = new AuthorDao();
@@ -118,9 +120,9 @@ public class AddControllers extends HttpServlet {
             product.setName(productName);
             product.setPrice(productPrice);
             product.setQuantity(productQuantity);
-            product.setDescription(description); 
+            product.setDescription(description);
             product.setCategoryId(categoryId);
-            product.setAuthorID(authorId); 
+            product.setAuthorID(authorId);
             product.setImgProduct(imgProduct);
             product.setAgeId(ageId);
 
@@ -133,8 +135,9 @@ public class AddControllers extends HttpServlet {
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
